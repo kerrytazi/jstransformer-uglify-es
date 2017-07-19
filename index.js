75d7fbbbfacd68b07b17338d77f523a2b81fb9e2
kerrytazi <kerrytazi@mail.ru>
2017-07-20 05:17:38 +0900

    fix

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ let fs     = require("fs");
 
 
 let render = function(code, options={}) {
-    return uglify.minify(code, options).code;
+    let result = uglify.minify(code, options);
+    return result.code || throw new Error(result.error);
 }
 
 
 let renderFile = function(path, options={}) {
-    const code = fs.readFileSync("test.js", "utf8");
-    return uglify.minify(code, options).code;
+    let code = fs.readFileSync("test.js", "utf8");
+    let result = uglify.minify(code, options);
+    return result.code || throw new Error(result.error);
 }
 
 
 module.exports = Object.assign(Object.create(null), {
     name: "uglify-es",
     outputFormat: "js",
-    inputFormat: ["js"],
+    inputFormats: ["uglify-es", "jsmin", "js"],
     render,
     renderFile
 });
